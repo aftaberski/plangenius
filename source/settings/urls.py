25330c4c356c7bd7e5890f56bc88fa2a9dce6dbe
Anna Taberski <aftaberski@gmail.com>
2015-10-03 14:47:18 -0400

    Add homepage

--- source/settings/urls.py
+++ source/settings/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^sign-up/$', views.sign_up, name='sign-up')
+    url(r'^sign-up/$', views.sign_up, name='sign-up'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^$', views.index, name='index'),
 ]
